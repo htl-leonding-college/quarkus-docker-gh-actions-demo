bed0abda055e600ab5b0e00dca10bcc806bdc918
htl-leonding <t.stuetz@htl-leonding.ac.at>
2021-03-14 11:54:01 +0100

    initial commit

---
+++ src/test/java/at/htl/vehicle/boundary/NativeVehicleEndpointIT.java
@@ -0,0 +1,9 @@
+package at.htl.vehicle.boundary;
+
+import io.quarkus.test.junit.NativeImageTest;
+
+@NativeImageTest
+public class NativeVehicleEndpointIT extends VehicleEndpointTest {
+
+    // Execute the same tests but in native mode.
+}
